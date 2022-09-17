d2c048dcabc4d9de79f4ac88df00feafd9497759
CoCarrivale <77854372+CoCarrivale@users.noreply.github.com>
2022-09-17 12:57:21 +0200

    Update config.sh

--- test/config.sh
+++ test/config.sh
@@ -4,3 +4,28 @@ cmake ../
 make
 make install
 cd ..
+
+mkdir data
+mkdir histos4datacards
+mkdir histos
+mkdir prova_histo
+cd histos
+mkdir deltaEtajj
+mkdir deltaPhijj
+mkdir MET
+mkdir M1T
+mkdir Mo1
+mkdir mll
+mkdir mjj
+mkdir Zeppenfeld_1
+mkdir Zeppenfeld_2
+mkdir Zeppenfeld_12
+mkdir Eta_l1
+mkdir Eta_l2
+mkdir Eta_j1
+mkdir Eta_j2
+mkdir PT_l1
+mkdir PT_l2
+mkdir PT_j1
+mkdir PT_j2
+cd ..
